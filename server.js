0f23d2675aa60847bac906ce694bab825967bd9c
sna <steve.newstead@eduserv.org.uk>
2017-02-12 16:22:40 +0000

    just see if a simple node site works!

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-require('coffee-script/register');
-module.exports = require('hubot/bin/hubot.coffee');
+//require('coffee-script/register');
+//module.exports = require('hubot/bin/hubot.coffee');
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
+    
+}).listen(process.env.PORT || 8080);
