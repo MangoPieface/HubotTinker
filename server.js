f767ebf280f6bb1a8b83880948367a40a1f65db2
sna <steve.newstead@eduserv.org.uk>
2017-02-12 16:23:55 +0000

    Undo the last change as it does work

--- server.js
+++ server.js
@@ -1,11 +1,2 @@
-//require('coffee-script/register');
-//module.exports = require('hubot/bin/hubot.coffee');
-
-var http = require('http');
-
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+require('coffee-script/register');
+module.exports = require('hubot/bin/hubot.coffee');
